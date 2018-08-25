505bb1a4f2986718e13fa9456176f09e25e98d24
Sasidhar Rachuri <srachuri@qti.qualcomm.com>
2018-08-25 13:10:14 +0530

    Initial Commit

---
+++ src/main/java/com/example/SpringBootDockerElkExample/SpringBootDockerElkExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.SpringBootDockerElkExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerElkExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerElkExampleApplication.class, args);
+	}
+}
